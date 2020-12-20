86cb3aedc8877832493c11387bab471a3b022014
Jack Edwards <reiman2222@hotmail.com>
2020-12-19 19:47:10 -0500

    basic messaging capability, readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # honey-bot
 discord bot
+
+## Installation
+Requires python3
+requires packages dotenv and discord.py
+
+## On env file
+This bot expects a env file named `.env` to exist in root with the variable `DISCORD_TOKEN` that is the bots secret token.
+
+## How to run
+Run with copmmand `python3 bot.py`
---
+++ bot.py
@@ -0,0 +1,33 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN') 
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    # break to stop infinte spam
+    if message.author == client.user:
+        return
+
+    content = message.content.lower()
+    response = ''
+
+    if content == '!ping':
+        response = 'pong'
+    elif 'easy' in content:
+        response = 'Not as easy as shopping online with Honey.'
+    elif 'amazon' in content or 'newegg' in content:
+        response = 'Consider shopping online with Honey.'
+
+    await message.channel.send(response)
+
+client.run(TOKEN)
